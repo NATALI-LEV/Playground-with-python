8c0088c874ea417574ac29daf78a39ceac0db2ce
NATALI-LEV <natalillev@gmail.com>
2023-09-12 12:46:13 +0300

    added naruto to screen

--- NARUTO ON A RUN/naruto.py
+++ NARUTO ON A RUN/naruto.py
@@ -9,6 +9,69 @@ pygame.init()
 screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
 pygame.display.set_caption('Naruto')
 
+class Naruto:
+  
+    def __init__(self):
+        self.width = 44
+        self.height = 44
+        self.x = 10
+        self.y = 80
+        self.texture_num = 0
+        self.dy = 3
+        self.gravity = 1.2
+        self.onground = True
+        self.jumping = False
+        self.jump_stop = 10
+        self.falling = False
+        self.fall_stop = self.y
+        self.set_texture()
+        self.set_sound()
+        self.show()
+
+    def update(self, loops):
+        # jumping
+        if self.jumping:
+            self.y -= self.dy
+            if self.y <= self.jump_stop:
+                self.fall()
+        
+        # falling
+        elif self.falling:
+            self.y += self.gravity * self.dy
+            if self.y >= self.fall_stop:
+                self.stop()
+
+        # walking
+        elif self.onground and loops % 4 == 0:
+            self.texture_num = (self.texture_num + 1) % 3
+            self.set_texture()
+        
+
+    def show(self):
+        screen.blit(self.texture, (self.x, self.y))
+
+    def set_texture(self):
+        path = os.path.join(f'assets/images/naruto_stand.png')
+        self.texture = pygame.image.load(path)
+        self.texture = pygame.transform.scale(self.texture, (self.width, self.height))
+
+    def set_sound(self):
+        path = os.path.join('assets/sounds/jump.wav')
+        self.sound = pygame.mixer.Sound(path)
+
+    def jump(self):
+        self.sound.play()
+        self.jumping = True
+        self.onground = False
+
+    def fall(self):
+        self.jumping = False
+        self.falling = True
+
+    def stop(self):
+        self.falling = False
+        self.onground = True
+  
 class BG:
 
   def __init__(self, x):
@@ -36,6 +99,7 @@ class Game:
 
   def __init__(self):
     self.bg = [BG(x=0), BG(x=WIDTH)]
+    self.naruto = Naruto()
     self.speed = 0.5
   
 def main():
@@ -49,6 +113,7 @@ def main():
       bg.update(-game.speed)
       bg.show()
     
+    game.naruto.show()
     
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
