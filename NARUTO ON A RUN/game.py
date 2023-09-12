1cc932e7d479f8522893a7fe4d5ae899f71ffa97
NATALI-LEV <natalillev@gmail.com>
2023-09-12 11:47:19 +0300

    setting the background

--- NARUTO ON A RUN/game.py
+++ NARUTO ON A RUN/game.py
@@ -1,19 +1,40 @@
 import os
 import sys
-import math
-import random
 import pygame
 
 WIDTH = 623
 HEIGHT = 150
 
 pygame.init()
-pygame.mixer.init()
 screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
 pygame.display.set_caption('Naruto')
 
+class BG:
+  
+  def __init__(self):
+    self.width = WIDTH
+    self.height = HEIGHT
+    self.set_texture()
+    self.show()
 
+  def show(self):
+    screen.blit(self.texture, (0,0))
+
+  def set_texture(self):
+    path = os.path.join('assets/images/background.png')
+    self.texture = pygame.image.load(path)
+    self.texture = pygame.transform.scale(self.texture, (self.width, self.height))
+  
+  
+class Game:
+  
+  def __init__(self):
+    self.bg = BG()
+  
 def main():
+  
+  game = Game()
+  
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
