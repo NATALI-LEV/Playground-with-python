f10f81c2b5b4cb1add9b08a706a8263b3bb394b0
NATALI-LEV <natalillev@gmail.com>
2023-09-12 12:14:48 +0300

    set the scrolling background animation

--- NARUTO ON A RUN/naruto.py
+++ NARUTO ON A RUN/naruto.py
@@ -10,37 +10,49 @@ screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
 pygame.display.set_caption('Naruto')
 
 class BG:
-  
-  def __init__(self):
+
+  def __init__(self, x):
     self.width = WIDTH
     self.height = HEIGHT
+    self.x = x
+    self.y = 0
     self.set_texture()
     self.show()
 
+  def update(self, dx):
+    self.x += dx
+    if self.x <= -WIDTH:
+      self.x = WIDTH
+
   def show(self):
-    screen.blit(self.texture, (0,0))
+    screen.blit(self.texture, (self.x, self.y))
 
   def set_texture(self):
     path = os.path.join('assets/images/background.png')
     self.texture = pygame.image.load(path)
     self.texture = pygame.transform.scale(self.texture, (self.width, self.height))
-  
-  
+
 class Game:
-  
+
   def __init__(self):
-    self.bg = BG()
+    self.bg = [BG(x=0), BG(x=WIDTH)]
+    self.speed = 0.5
   
 def main():
   
-  game = Game()
-  
+  game = Game()  
   while True:
+    
+    for bg in game.bg:
+      bg.update(-game.speed)
+      bg.show()
+    
+    
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         pygame.quit()
         sys.exit()
         
-    pygame.display.update
+    pygame.display.update()
 
 main()
