68bb0c11b422cb3b4cfbca829e14cea343d84949
NATALI-LEV <natalillev@gmail.com>
2023-10-01 17:50:58 +0300

    set the backgronud

--- Solar System/main.py
+++ Solar System/main.py
@@ -6,10 +6,14 @@ pygame.init()
 WIDTH, HEIGHT =  1000, 1000
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Solar Simulation")
+background = pygame.image.load('stars.jpg')
+background = pygame.transform.scale(background, (WIDTH, HEIGHT))
 
 def main():
     run = True
-    
+    WIN.blit(background, (0, 0))
+    pygame.display.update() 
+
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
